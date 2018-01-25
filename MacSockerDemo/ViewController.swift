36ee6197b6de3f333b27571171f1720502e7f64f
huangshupeng <825844231@qq.com>
2018-01-25 14:38:05 +0800

    Initial Commit

---
+++ MacSockerDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacSockerDemo
+//
+//  Created by okdeer on 2018/1/25.
+//  Copyright © 2018年 okdeer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
